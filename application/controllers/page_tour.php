aca008725704741acc0acf06091cb9371db61d4a
M Junio Azhari <45429901+junioazhari@users.noreply.github.com>
2021-09-10 21:37:26 +0700

    mengubah nama file pada controllers

--- application/controllers/page_tour.php
+++ application/controllers/page_tour.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Web extends CI_Controller
+class page_tour extends CI_Controller
 {
 
   function __construct()
@@ -13,6 +13,12 @@ class Web extends CI_Controller
   public function index()
   {
     $data['judul'] = "Paket Tour";
-    $this->load->view('v_index', $data);
+    $this->load->view('v_tour', $data);
   }
+  public function artikel(){		
+		$data['judul'] = "Halaman artikel";
+		$this->load->view('v_artikel',$data);
+		
+	}
 }
+
